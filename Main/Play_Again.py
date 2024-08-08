b003595d52fd54870ef5d7fb93d616fe392adccc
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-08 11:46:03 +0200

    Start to write the code and committed to update all versione in the variuos enviroments

---
+++ Main/Play_Again.py
@@ -0,0 +1,4 @@
+# This file is used for the function Play_Again()
+
+def Play_Again():
+     pass
---
+++ Main/main.py
@@ -0,0 +1,10 @@
+# This is the main scope about this project
+
+
+print("Hello gamers, here a simple Quiz game thought to entretainement you.\nEnjoy it!!!")
+
+question = {
+     "Who is the first American's President?" : "B",
+     "When was the second world war?" : "A",
+     "In which country was intented the pizza?" : "C"    
+}
