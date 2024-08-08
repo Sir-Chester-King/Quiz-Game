667ea34ca9617cf744704e8dba422d17eec14c1e
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-08 16:49:31 +0200

    Just added a comment

--- Main/New_Game.py
+++ Main/New_Game.py
@@ -1,12 +1,21 @@
 # This is the scope for the new game.
 import os
 
+
 # This function just clear the console interpreter to a fancy view.
 def clear_console():
-     os.system('clear')
+    os.system('clear')
+
 
 def new_game(questions, number_correct_answer, *correct_answer):
+     number_questions = len(questions)
+     print(questions.items())
+     for key in questions.keys():
+          print("Question number {}: \n")
+
      for index in correct_answer:
           print("List of correct anwer is: ", index, end="\n")
           #print("Correct answer number {}: ".format(index))
-
+     for key,value in questions.items():
+          print(key)
+          print(value)
