51bb5c185662bf505a850231fba2c70c5e0e79f1
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-08 11:59:12 +0200

    Update main.py
    Updated main

--- Main/main.py
+++ Main/main.py
@@ -1,10 +1,30 @@
 # This is the main scope about this project
 
 
-print("Hello gamers, here a simple Quiz game thought to entretainement you.\nEnjoy it!!!")
+print("Hello gamer, here a simple Quiz game thought to entretainement you.\nEnjoy it!!!")
 
 question = {
      "Who is the first American's President?" : "B",
      "When was the second world war?" : "A",
      "In which country was intented the pizza?" : "C"    
+}
+
+question_1 = {
+     "A" : "Joe Biden",
+     "B" : "George Whashington",
+     "C" : "Barack Obama",
+     "D" : "John Kennedy"
+}
+question_2 = {
+     "A" : "1940",
+     "B" : "2001",
+     a
+     "C" : "1390",
+     "D" : "1930"
+}
+question_3 = {
+     "A" : "France",
+     "B" : "United Kingdom",
+     "C" : "Italy",
+     "D" : "America"
 }
